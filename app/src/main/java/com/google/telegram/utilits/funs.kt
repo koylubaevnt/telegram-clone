a1c0db999731caf3538ccf2aaf82ee23b2d75538
Nikita Koylubaev <NKoylubaev@ibs.ru>
2020-05-27 10:47:01 +0500

    38. Поиск и устранение багов.

--- app/src/main/java/com/google/telegram/utilits/funs.kt
+++ app/src/main/java/com/google/telegram/utilits/funs.kt
@@ -2,12 +2,14 @@ package com.google.telegram.utilits
 
 import android.content.Context
 import android.content.Intent
+import android.provider.ContactsContract
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.telegram.R
+import com.google.telegram.models.CommonModel
 import com.squareup.picasso.Picasso
 
 fun showToast(message: String) {
@@ -60,4 +62,31 @@ fun ImageView.downloadAndSetImage(url: String) {
         .fit()
         .placeholder(R.drawable.default_photo)
         .into(this)
+}
+
+
+fun initContacts() {
+    if (checkPermission(READ_CONTACTS)) {
+        val arrayContacts = arrayListOf<CommonModel>()
+        val cursor = APP_ACTIVITY.contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+        cursor?.let {
+            while (it.moveToNext()) {
+                val fullName = it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val phone = it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                val newModel = CommonModel()
+                newModel.fullname = fullName
+                newModel.phone = phone.replace(Regex("[\\s,-]"), "")
+                arrayContacts.add(newModel)
+            }
+        }
+        cursor?.close()
+
+        updatePhonesToDatabase(arrayContacts)
+    }
 }
