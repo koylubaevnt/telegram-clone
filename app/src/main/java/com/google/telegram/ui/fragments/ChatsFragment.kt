4997781438727530196658ead66a11365e9458fa
Nikita Koylubaev <NKoylubaev@ibs.ru>
2020-05-28 12:16:23 +0500

    39. Отправка сообщений в Firebase.

--- app/src/main/java/com/google/telegram/ui/fragments/ChatsFragment.kt
+++ app/src/main/java/com/google/telegram/ui/fragments/ChatsFragment.kt
@@ -2,7 +2,13 @@ package com.google.telegram.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.google.telegram.R
+import com.google.telegram.utilits.APP_ACTIVITY
 
 class ChatsFragment : Fragment(R.layout.fragment_chat) {
+
+    override fun onResume() {
+        super.onResume()
+        APP_ACTIVITY.title = "Чаты"
+    }
 
 }
--- app/src/main/java/com/google/telegram/ui/fragments/SingleChatFragment.kt
+++ app/src/main/java/com/google/telegram/ui/fragments/SingleChatFragment.kt
@@ -7,6 +7,7 @@ import com.google.telegram.models.CommonModel
 import com.google.telegram.models.UserModel
 import com.google.telegram.utilits.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_single_chat.*
 import kotlinx.android.synthetic.main.toolbar_info.view.*
 
 class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layout.fragment_single_chat) {
@@ -27,6 +28,18 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
 
         mRefUser = REF_DATABASE_ROOT.child(NODE_USERS).child(contact.id)
         mRefUser.addValueEventListener(mListenerInfoToolbar)
+
+        chat_btn_send_message.setOnClickListener {
+            val message = chat_input_message.text.toString()
+            if (message.isEmpty()) {
+                showToast("Введите сообщение")
+            } else {
+                sendMessage(message, contact.id, TYPE_TEXT) {
+                    chat_input_message.setText("")
+                }
+            }
+        }
+
     }
 
     private fun initInfoToolbar() {
